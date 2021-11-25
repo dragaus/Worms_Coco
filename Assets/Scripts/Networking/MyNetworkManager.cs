48bce9f049b530c03de4096a288dfc362c8b5726
dragaus <dragalichi@gmail.com>
2021-11-25 09:00:32 -0600

    Send Messages

--- Assets/Scripts/Networking/MyNetworkManager.cs
+++ Assets/Scripts/Networking/MyNetworkManager.cs
@@ -19,21 +19,57 @@ public class MyNetworkManager : NetworkManager
         coins[index] += coinsToAdd;
     }
 
+    public struct NameMessage : NetworkMessage
+    {
+        public string userName;
+    }
+
+    public override void OnServerConnect(NetworkConnection conn)
+    {
+        Debug.Log("Server connects a client");
+        base.OnServerConnect(conn);
+        NetworkServer.RegisterHandler<NameMessage>(OnGetMessage, false);
+    }
+
+    public void OnGetMessage(NetworkConnection conn, NameMessage msg)
+    {
+        playerNames.Add(msg.userName);
+
+        if (!conn.isReady) return;
+
+        if (!conn.identity.TryGetComponent(out MyNetworkPlayer player))
+        {
+            return;
+        }
+
+        player.SetDisplayName(playerNames[numPlayers - 1]);
+    }
+
     public override void OnClientConnect(NetworkConnection conn)
     {
+        Debug.Log("Client is connected");
         base.OnClientConnect(conn);
 
+        var msg = new NameMessage()
+        {
+            userName = field.text
+        };
+
+        NetworkClient.Send(msg);
     }
 
     public override void OnServerAddPlayer(NetworkConnection conn)
     {
         base.OnServerAddPlayer(conn);
-        Debug.Log($"Se agrega juagdor {conn.connectionId}");
+        Debug.Log($"Server add player");
 
         var newPlayer = conn.identity.GetComponent<MyNetworkPlayer>();
 
-        newPlayer.SetDisplayName($"Player {numPlayers}");
-        playerNames.Add($"Player {numPlayers}");
+        if (playerNames.Count == numPlayers)
+        {
+            newPlayer.SetDisplayName(playerNames[numPlayers - 1]);
+        }
+
         coins.Add(10);
 
         var instace = Instantiate(players[numPlayers - 1], conn.identity.transform);
